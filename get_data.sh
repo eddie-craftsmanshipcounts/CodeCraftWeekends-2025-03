cd614eb519890c33cbbff25b20e6d1da759ccfa3
Eddie Bush <eddie@craftsmanshipcounts.com>
2025-03-09 16:03:35 +0000

    Add a way to get our source data and put it where we expect it for processing.

---
+++ get_data.sh
@@ -0,0 +1,6 @@
+rm -rf data
+mkdir data
+cd data
+wget https://download.hcad.org/data/CAMA/2024/Real_acct_owner.zip
+unzip *.zip
+rm *.zip
---
+++ src/hcad_star_schema/test_it_works.py
@@ -0,0 +1,5 @@
+import unittest
+
+class TestItWorks(unittest.TestCase):
+    def test_it_works(self):
+        self.assertTrue(True)
